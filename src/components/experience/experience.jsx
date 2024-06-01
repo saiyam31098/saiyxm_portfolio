576362e24b1db8b890a227694d0ad28c0706c74f
saija <saija@akamai.com>
2024-06-01 15:26:29 +0530

    Added company logo in experience section

--- src/components/experience/experience.jsx
+++ src/components/experience/experience.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState, useContext } from 'react';
 import './experience.css';
 import { FaLaptopCode } from "react-icons/fa";
+import AKAM_LOGO from '../../assets/Akamai.svg';
+import CAPG_LOGO from '../../assets/capgemini-logo.svg';
 
 const Experience = () => {
     return (
@@ -9,22 +11,36 @@ const Experience = () => {
             <h5 className="work__history">Work History</h5>
             <h2 className="experience__header">Experience</h2>
             <div class="timeline">
-                <div class="container left">
-                    <div class="date">Feb 2022 - Present</div>
+                <div class="container right">
+                    <div class="date">July 2024 - Present</div>
                     <i class="icon"><FaLaptopCode /></i>
                     <div class="content">
-                        <h2>Akamai Technologies</h2>
-                        <h5 className="job__position">Software Engineer II</h5>
-                        <p> Build and develop complex and critical solutions by collaborating with cross-functional teams including developers, testers, designers and Program Managers. 
-                            <br/>Stack used: Apex, LWC, Aura, Mulesoft, Data Loader, Big Panda, React.
+                    <center><img src={AKAM_LOGO} style={{ height: '25%', width: '25%' }}></img></center>
+                        {/* <h2>Akamai Technologies</h2> */}
+                        <h5 className="job__position">Upcoming - Senior Software Engineer</h5>
+                        <p> TBD after July month.
+                            <br/>Stack used: Apex, LWC, Aura, Admin, Data Loader.
                         </p>
+                    </div>
+                </div>
+                <div class="container left">
+                    <div class="date">Feb 2022 - June 2024</div>
+                    <i class="icon"><FaLaptopCode /></i>
+                    <div class="content">
+                        <center><img src={AKAM_LOGO} style={{ height: '25%', width: '25%' }}></img></center>
+                        {/* <h2>Akamai Technologies</h2> */}
+                        <h5 className="job__position">Software Engineer II</h5>
+                        <center><p> Build and develop complex and critical solutions by collaborating with cross-functional teams including developers, testers, designers and Program Managers. 
+                            <br/>Stack used: Apex, LWC, Aura, Mulesoft, Data Loader, Big Panda, React.
+                        </p></center>
                     </div>
                 </div>
                 <div class="container right">
                     <div class="date">Sept 2020 - Feb 2022</div>
                     <i class="icon"><FaLaptopCode /></i>
                     <div class="content">
-                        <h2>Capgemini India Pvt Ltd</h2>
+                        <center><img src={CAPG_LOGO} style={{ height: '60%', width: '60%' }}></img></center>
+                        {/* <h2>Capgemini India Pvt Ltd</h2> */}
                         <h5 className="job__position">Senior Analyst</h5>
                         <p> Worked as a Salesforce Developer and developed and impelemented multiple solutions for one of the largest beverage manufactures in the world. Built multiple automations which helped reduce manual work. 
                             <br/>Stack used: Apex, LWC, Aura, Admin, Data Loader.
@@ -35,7 +51,8 @@ const Experience = () => {
                     <div class="date">Jan 2020 - Mar 2020</div>
                     <i class="icon"><FaLaptopCode /></i>
                     <div class="content">
-                        <h2>Capgemini India Pvt Ltd</h2>
+                        <center><img src={CAPG_LOGO} style={{ height: '60%', width: '60%' }}></img></center>
+                        {/* <h2>Capgemini India Pvt Ltd</h2> */}
                         <h5 className="job__position">Senior Analyst Intern</h5>
                         <p> Trained on Salesforce, Java, SQL, Angular. Developed and collaborated as a team to build projects such as Order Management System.</p>
                     </div>
